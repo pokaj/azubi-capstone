77490bccf93b16eccb466a78bf1662bb4ebcda43
Vincent <vincent.aboagye@azubiafrica.org>
2020-09-02 17:46:09 +0000

    Comment out validation rules in the registration form

--- frontend/events_capstone/src/App.js
+++ frontend/events_capstone/src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LOGINOUTLAYOUT from './components/LOGINOUTLAYOUT';
-import Registration_form from './components/registration_form';
+import RegistrationForm from './components/registration_form';
 
 function App() {
   return (
     <React.Fragment>
-    <Registration_form/>
+    <LOGINOUTLAYOUT/>
     </React.Fragment>
     
     
