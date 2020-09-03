bc12c7f00b11a89a2853fabdd14dff4dfeda9c84
Afi <afimaamedufie@gmail.com>
2020-09-03 13:09:50 +0000

    Added login endpoint

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('api/logout/', knox_views.LogoutView.as_view(), name='logout'),
     path('api/logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
     path('api/register/', RegisterAPI.as_view(), name='register'),
+    path('api/login/', LoginAPI.as_view(), name='login'),
 ]
