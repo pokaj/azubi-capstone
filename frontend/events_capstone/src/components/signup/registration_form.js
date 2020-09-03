71c41e1b20a748e638dfea821574453dd220e11f
alhassanmoses <alhassanmoses.amw@gmail.com>
2020-09-03 03:46:28 +0000

    fixed validation bugs and improved css for registration form

--- frontend/events_capstone/src/components/signup/registration_form.js
+++ frontend/events_capstone/src/components/signup/registration_form.js
@@ -1,19 +1,25 @@
 //Event registration app registration component
 
 //import dependencies
-import React, { Component } from "react";
+import React, { Component, useRef } from "react";
 import { useForm } from "react-hook-form";
 
 //module imports
 import "./styles.css";
+import ErrorMessage from "./errorMessages";
 
 //registration function component
 const MainForm = () => {
   //destructuring feature methods from useForm() - "react-use-form"
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, errors, watch } = useForm();
+
+  //keeping watch for the password form field to make comparison with the confirm password field
+  const password = useRef({});
+  password.current = watch("password", "");
 
   //function to handle form submission
   const onSubmit = (data) => {
+    alert(JSON.stringify(data));
     console.log(data);
   };
 
@@ -23,33 +29,38 @@ const MainForm = () => {
       <form className="App" onSubmit={handleSubmit(onSubmit)}>
         <h1>Registration</h1>
         <label>First Name:</label>
-        <input name="firstName" ref={register({ require: true, min: 2 })} />
-        {/*
-    {error.firstName && error.firstName.type === "min" && <p>You need a minimum of 2 characters</p>} */}
+        <input
+          name="firstName"
+          ref={register({ required: true, minLength: 2 })}
+        />
+        <ErrorMessage error={errors.firstName} />
 
         <label>Last Name:</label>
-        <input name="lastName" ref={register({ require: true, min: 2 })} />
-        {/*
-    {error.lastName && error.lastName.type === "min" && <p>You need a minimum of 2 characters</p>} */}
+        <input
+          name="lastName"
+          ref={register({ required: true, minLength: 2 })}
+        />
+        <ErrorMessage error={errors.lastName} />
 
         <label>Email</label>
         <input
           name="email"
           ref={register({
-            require: true,
+            required: true,
             pattern: {
               value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
               message: "invalid email address",
             },
           })}
         />
+        <ErrorMessage error={errors.email} />
 
         <label>Password</label>
         <input
           name="password"
           type="password"
           ref={register({
-            require: true,
+            required: true,
             pattern: {
               value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{3,})/i,
               message: "invalid password",
@@ -61,39 +72,57 @@ const MainForm = () => {
         <input
           name="confirmPassword"
           type="password"
-          ref={register({ require: true })}
+          ref={register({
+            required: true,
+            validate: (value) =>
+              value === password.current || "Passwords do not match.",
+          })}
         />
-        {/* validate: (value) => value === this.watch('password') || "Passwords don't match." })}/> */}
+        <ErrorMessage error={errors.confirmPassword} />
 
         <label>Address</label>
-        <input name="address" ref={register({ require: true, min: 4 })} />
-        {/*
-    {error.address && error.address.type === "min" && <p>You need a minimum of 4 characters for an address</p>} */}
+        <input
+          name="address"
+          ref={register({ required: true, minLength: 4 })}
+        />
+        <ErrorMessage error={errors.address} />
 
         <label>Phone Number</label>
-        <input name="phoneNumber" ref={register({ require: true })} />
-        {/* message: "invalid phone number"} */}
+        <input
+          name="phoneNumber"
+          ref={register({ required: true, minLength: 8 })}
+        />
+        {errors.phoneNumber && <p>You need a minimum of 8 digits</p>}
 
         <label>City</label>
-        <select name="city" ref={register({ require: true })}>
+        <select
+          name="city"
+          className="options"
+          ref={register({ required: true })}
+        >
           <option value="">City...</option>
           <option value="tamale">Tamale</option>
           <option value="accra">Accra</option>
           <option value="kumasi">kumasi</option>
         </select>
+        <ErrorMessage error={errors.city} />
 
         <span>
-          <input
-            type="checkbox"
-            name="terms"
-            ref={register({
-              require: true,
-              // validator: (accepted) => this.event.target.checked,
-            })}
-          />
-          <label>I agree with the terms and conditions.</label>
-          {/* {error.checkbox && error.city.type === "require" && <p>You need to accept the terms and conditions to proceed</p>} */}
+          <label className="checkbox">
+            <input
+              type="checkbox"
+              name="terms"
+              ref={register({
+                required: true,
+              })}
+            />
+            I agree with the terms and conditions.
+          </label>
         </span>
+        {/* <ErrorMessage error={errors.terms} /> */}
+        {errors.terms && (
+          <p>You need to accept the terms and conditions to proceed.</p>
+        )}
 
         <button type="submit">Confirm Registration</button>
       </form>
